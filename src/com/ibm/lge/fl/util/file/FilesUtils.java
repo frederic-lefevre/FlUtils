0d0dac5bf694bf5ef717679f406fdebf37844b9a
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2018-10-03 07:54:30 +0200

    Move FilesUtils methods related to security to a new class

--- src/com/ibm/lge/fl/util/file/FilesUtils.java
+++ src/com/ibm/lge/fl/util/file/FilesUtils.java
@@ -22,24 +22,18 @@ import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.DosFileAttributeView;
 import java.nio.file.attribute.DosFileAttributes;
-import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.FileTime;
 import java.nio.file.attribute.GroupPrincipal;
 import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.nio.file.attribute.UserPrincipal;
-import java.nio.file.attribute.UserPrincipalLookupService;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
@@ -64,51 +58,6 @@ public class FilesUtils {
 		return deleteDirVisitor.isSuccessful() ;
 	}
 	
-
-	
-	// Set permission for a file.
-	// If it does not exists the file is created with the given permission
-	public static void setFilePermission(Path fPath, String permissions, Logger pLog) {
-		setPermission(false, fPath, permissions, pLog) ;
-	}
-	
-	// Set permission for a directory.
-	// If it does not exists the directory is created with the given permission
-	public static void setDirectoryPermission(Path fPath, String permissions, Logger pLog) {
-		setPermission(true, fPath, permissions, pLog) ;
-	}
-	
-	private static void setPermission(boolean isDirectory, Path fPath, String permissions, Logger pLog) {
-		
-		try {
-			
-			FileStore fileStore = Files.getFileStore(fPath) ;
-		
-			if (fileStore.supportsFileAttributeView(PosixFileAttributeView.class)) {
-				FileAttribute<Set<PosixFilePermission>> fileAttributes = null ;
-				Set<PosixFilePermission> perms = null;
-				if ((permissions != null) && (! permissions.isEmpty())) {
-					
-						perms = PosixFilePermissions.fromString(permissions);
-						fileAttributes = PosixFilePermissions.asFileAttribute(perms);
-						
-						if (Files.exists(fPath))  {
-							Files.setPosixFilePermissions(fPath, perms) ;
-						} else if (isDirectory) {
-							Files.createDirectory(fPath, fileAttributes) ;
-						} else {
-							Files.createFile(fPath, fileAttributes) ;
-						}
-						
-					
-				}
-			}
-		}catch (IOException e) {
-			pLog.log(Level.SEVERE, "IOException when creating a policy file " + fPath + " with permission parameter " + permissions, e) ;
-		} catch (Exception e) {
-			pLog.log(Level.SEVERE, "Exception when parsing posix file permission parameter: " + permissions, e) ;
-		}
-	}
 	
 	private static class DeleteDirectoryVisitor extends SimpleFileVisitor<Path> {
 		
@@ -131,7 +80,7 @@ public class FilesUtils {
 				// if "force" try to set the file writable and retry the delete
 				if (force) {
 					try {
-						setWritable(file) ;	
+						FilesSecurityUtils.setWritable(file) ;	
 						Files.delete(file);
 					} catch (Exception e1) {
 						success = false ;
@@ -157,7 +106,7 @@ public class FilesUtils {
 					// if "force" try to set the file writable and retry the delete
 					if (force) {
 						try {
-							setWritable(dir) ;	
+							FilesSecurityUtils.setWritable(dir) ;	
 							Files.delete(dir);
 						} catch (Exception e2) {
 							success = false ;
@@ -231,51 +180,7 @@ public class FilesUtils {
 		}
 	}
 	
-	public static void setWritable(Path path) throws IOException {
 
-		FileStore fileStore = Files.getFileStore(path) ;
-		
-		// Get the UserPrincipal of the running process
-		UserPrincipalLookupService upls = path.getFileSystem().getUserPrincipalLookupService();
-		UserPrincipal user = upls.lookupPrincipalByName(System.getProperty("user.name"));
-
-		if (fileStore.supportsFileAttributeView(AclFileAttributeView.class)) {
-			
-			AclFileAttributeView aclAttr = Files.getFileAttributeView(path, AclFileAttributeView.class);
-		
-			AclEntry.Builder builder = AclEntry.newBuilder();
-			builder.setPermissions(EnumSet.of(AclEntryPermission.READ_DATA, AclEntryPermission.EXECUTE,
-					AclEntryPermission.READ_ACL, AclEntryPermission.READ_ATTRIBUTES, AclEntryPermission.READ_NAMED_ATTRS,
-					AclEntryPermission.WRITE_ACL, AclEntryPermission.DELETE, AclEntryPermission.WRITE_DATA,  AclEntryPermission.APPEND_DATA,
-					 AclEntryPermission.WRITE_OWNER,  AclEntryPermission.WRITE_ATTRIBUTES,  AclEntryPermission.WRITE_NAMED_ATTRS));
-			builder.setPrincipal(user);
-			builder.setType(AclEntryType.ALLOW);
-			aclAttr.setAcl(Collections.singletonList(builder.build()));
-		}
-		
-		if (fileStore.supportsFileAttributeView(PosixFileAttributeView.class)) {
-			
-			Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
-			// add owners permission
-			perms.add(PosixFilePermission.OWNER_READ);
-			perms.add(PosixFilePermission.OWNER_WRITE);
-			perms.add(PosixFilePermission.OWNER_EXECUTE);
-			// add group permissions
-			perms.add(PosixFilePermission.GROUP_READ);
-			perms.add(PosixFilePermission.GROUP_WRITE);
-			perms.add(PosixFilePermission.GROUP_EXECUTE);
-			// add others permissions
-			perms.add(PosixFilePermission.OTHERS_READ);
-			perms.add(PosixFilePermission.OTHERS_WRITE);
-			perms.add(PosixFilePermission.OTHERS_EXECUTE);
-			Files.setPosixFilePermissions(path, perms);
-		} 
-		
-		if (fileStore.supportsFileAttributeView(FileOwnerAttributeView.class)) {
-			FileOwnerAttributeView ownerView = Files.getFileAttributeView(path, FileOwnerAttributeView.class);
-			ownerView.setOwner(user) ;
-		}
-	}
 	
 	public static FileStore getFileStoreInformation(Path path, StringBuilder fsInfos, Logger logger) {
 		
