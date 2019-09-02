c4fe2bf74220ba5f7542e9a489420ea675d3fc82
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-09-02 07:12:25 +0200

    Create a log display panel

---
+++ src/main/java/com/ibm/lge/fl/util/swing/LogsDisplayPane.java
@@ -0,0 +1,44 @@
+package com.ibm.lge.fl.util.swing;
+
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class LogsDisplayPane  extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+
+	private final JScrollPane 		 scrollInfos ;
+	private final TextAreaLogHandler logTextAreaHandler ;
+
+	public LogsDisplayPane(Logger logger) {
+		super();
+		
+		setBorder(BorderFactory.createLineBorder(Color.BLACK,5,true)) ;
+		
+		JTextArea logArea = new JTextArea(40, 120) ;
+		logArea.setEditable(false);
+		logArea.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		
+		scrollInfos = new JScrollPane(logArea) ;
+		
+		logTextAreaHandler = new TextAreaLogHandler(logArea) ;
+		logTextAreaHandler.setLevel(logger.getLevel());
+		logger.addHandler(logTextAreaHandler);
+		
+		add(scrollInfos) ;
+	}
+	
+	public void setHighLightColor(Color color) {
+		logTextAreaHandler.setHighLightColor(color) ;
+	}
+	
+	public void setLastNonHighLighedLevel(Level level) {
+		logTextAreaHandler.setLastNonHighLighedLevel(level) ;
+	}
+}
