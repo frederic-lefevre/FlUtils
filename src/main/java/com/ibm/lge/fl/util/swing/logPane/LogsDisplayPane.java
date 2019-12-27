7e81364a8db84f90c87e105f13b466564e9ba720
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-12-27 19:22:35 +0100

    Display old logs in tab

--- src/main/java/com/ibm/lge/fl/util/swing/logPane/LogsDisplayPane.java
+++ src/main/java/com/ibm/lge/fl/util/swing/logPane/LogsDisplayPane.java
@@ -4,11 +4,9 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
-public class LogsDisplayPane  extends JPanel {
+public class LogsDisplayPane  extends JTabbedPane {
 
 	private static final long serialVersionUID = 1L;
 
@@ -18,22 +16,26 @@ public class LogsDisplayPane  extends JPanel {
 	private int currentLogDisplayIndex ;
 	private SearchableLogDisplay currentLogDisplay ;
 	
+	private Color logTabSelectedColor = Color.GREEN ;
+	private Color logTabRegularColor ;
+
 	private final static int LOG_DISPLAY_NUMBER = 2 ;
 	
 	public LogsDisplayPane(int level, Color color, Logger logger) {
 		
 		super();
-		setLayout(new BoxLayout(this,  BoxLayout.X_AXIS)) ;
-		setBorder(BorderFactory.createLineBorder(Color.BLACK,5,true)) ;
 		
 		searchableLogDisplays = new ArrayList<SearchableLogDisplay>() ;
 		for (int i=0; i < LOG_DISPLAY_NUMBER; i++) {
-			searchableLogDisplays.add(new SearchableLogDisplay(level, color, logger)) ;
+			SearchableLogDisplay logDisplay = new SearchableLogDisplay(level, color, logger) ;
+			searchableLogDisplays.add(logDisplay) ;
+			add(logDisplay.getPanel()) ;
 		}
 		currentLogDisplayIndex = 0 ;
 		currentLogDisplay = searchableLogDisplays.get(currentLogDisplayIndex) ;
 
-		add(currentLogDisplay.getPanel()) ;
+		logTabRegularColor = getBackgroundAt(0) ;
+		selectCurrentLogDisplay() ;
 		
 		logTextAreaHandler = new TextAreaLogHandler(currentLogDisplay, new SearchLogDisplayChanger()) ;
 		logTextAreaHandler.setLevel(logger.getLevel());
@@ -54,15 +56,22 @@ public class LogsDisplayPane  extends JPanel {
 		currentLogDisplay.refreshLogRecordCategories();
 	}
 	
+	private void selectCurrentLogDisplay() {
+		setSelectedIndex(currentLogDisplayIndex) ;
+		int logTabIdx = indexOfComponent(currentLogDisplay.getPanel()) ;
+		setBackgroundAt(logTabIdx,logTabSelectedColor) ;
+	}
+	
 	private class SearchLogDisplayChanger implements LogDisplayChanger {
 
 		@Override
 		public LogDisplayComponent changeLogDisplayComponent() {
-			remove(currentLogDisplay.getPanel());
+			int logTabIdx = indexOfComponent(currentLogDisplay.getPanel()) ;
+			setBackgroundAt(logTabIdx,logTabRegularColor) ;
 			currentLogDisplayIndex = (currentLogDisplayIndex + 1) % LOG_DISPLAY_NUMBER ;
 			currentLogDisplay = searchableLogDisplays.get(currentLogDisplayIndex) ;
 			currentLogDisplay.clear();
-			add(currentLogDisplay.getPanel()) ;
+			selectCurrentLogDisplay() ;
 			return currentLogDisplay ;
 		}
 		
