33a015a80a2f4c29324f568e7d45c3b46a91ad46
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-12-28 11:44:00 +0100

    Color parsing helper

---
+++ src/test/java/com/ibm/lge/fl/util/swing/ColorHelpersTests.java
@@ -0,0 +1,58 @@
+package com.ibm.lge.fl.util.swing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+import org.junit.jupiter.api.Test;
+
+class ColorHelpersTests {
+
+	@Test
+	void testBlue() {
+
+		Color c = ColorHelpers.parse("BLUE") ;
+		assertEquals(c, Color.BLUE) ;
+		
+		c = ColorHelpers.parse("blue") ;
+		assertEquals(c, Color.blue) ;
+
+		c = ColorHelpers.parse("Blue") ;
+		assertNull(c) ;
+	}
+
+	@Test
+	void testGreen() {
+
+		Color c = ColorHelpers.parse("GREEN") ;
+		assertEquals(c, Color.GREEN) ;
+		
+		c = ColorHelpers.parse("green") ;
+		assertEquals(c, Color.green) ;
+
+		c = ColorHelpers.parse("Green") ;
+		assertNull(c) ;
+	}
+	
+	@Test
+	void testWrongString() {
+
+		Color c = ColorHelpers.parse("WRONG") ;
+		assertNull(c) ;
+	}
+	
+	@Test
+	void testRGB() {
+
+		Color c = ColorHelpers.parse("#C6C6C6") ;
+		assertNotNull(c) ;
+	}
+	
+	@Test
+	void testRGB2() {
+
+		Color c = ColorHelpers.parse("0") ;
+		assertNotNull(c) ;
+		assertEquals(c, Color.black) ;
+	}
+}
