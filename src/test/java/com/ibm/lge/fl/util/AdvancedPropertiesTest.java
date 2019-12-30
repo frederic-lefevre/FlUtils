9d97192c53b60a2a655f8b26513c42c5323303b1
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-12-30 08:42:41 +0100

    Add tests for getKeyElements

--- src/test/java/com/ibm/lge/fl/util/AdvancedPropertiesTest.java
+++ src/test/java/com/ibm/lge/fl/util/AdvancedPropertiesTest.java
@@ -2,6 +2,7 @@ package com.ibm.lge.fl.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,6 +10,61 @@ import org.junit.jupiter.api.Test;
 
 class AdvancedPropertiesTest {
 
+	@Test
+	void testKeys() {
+		
+		AdvancedProperties advProps = new AdvancedProperties() ;
+		advProps.setProperty("a.b.c.k2.z", "s2") ;
+		advProps.setProperty("a.b.c.k1.e", "s1") ;
+		advProps.setProperty("a.b.c.k4", "s4") ;
+		advProps.setProperty("a.b.c.k3.t.v", "s3") ;
+		
+		assertEquals("s1", advProps.getProperty("a.b.c.k1.e")) ;
+		assertEquals("s2", advProps.getProperty("a.b.c.k2.z")) ;
+		assertEquals("s3", advProps.getProperty("a.b.c.k3.t.v")) ;
+		assertEquals("s4", advProps.getProperty("a.b.c.k4")) ;
+		
+		ArrayList<String> keys = advProps.getKeysElements("a.b.c.") ;
+		assertEquals(4, keys.size()) ;
+		assertEquals("k1", keys.get(0)) ;
+		assertEquals("k2", keys.get(1)) ;
+		assertEquals("k3", keys.get(2)) ;
+		assertEquals("k4", keys.get(3)) ;
+	}
+
+	@Test
+	void testKeys2() {
+		
+		AdvancedProperties advProps = new AdvancedProperties() ;
+		advProps.setProperty("p1", "10") ;	
+		
+		ArrayList<String> keys = advProps.getKeysElements("a.b.c.") ;
+		assertNotNull(keys) ;
+		assertEquals(0, keys.size()) ;
+		
+		keys = advProps.getKeysElements("a.b") ;
+		assertNotNull(keys) ;
+		assertEquals(0, keys.size()) ;
+		
+		keys = advProps.getKeysElements(".") ;
+		assertNotNull(keys) ;
+		assertEquals(0, keys.size()) ;
+		
+		keys = advProps.getKeysElements("") ;
+		assertNotNull(keys) ;
+		assertEquals(1, keys.size()) ;
+		assertEquals("p1", keys.get(0)) ;
+	}
+
+	@Test
+	void testKeys3() {
+		AdvancedProperties advProps = new AdvancedProperties() ;
+		advProps.setProperty("p1", "10") ;
+		
+		assertThrows(NullPointerException.class, () -> advProps.getKeysElements(null)) ;
+	}
+
+	
 	@Test
 	void testInt() {
 		
