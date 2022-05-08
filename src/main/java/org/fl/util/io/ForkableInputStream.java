26168bbf08f32d4dd56205d1354705776411ea49
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2022-05-08 09:29:38 +0200

    Update ForkableInputStream

--- src/main/java/org/fl/util/io/ForkableInputStream.java
+++ src/main/java/org/fl/util/io/ForkableInputStream.java
@@ -1,5 +1,6 @@
 package org.fl.util.io;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,8 +20,12 @@ public class ForkableInputStream extends InputStream {
 
     public ForkableInputStream(InputStream sourceInputStream, Logger l) {
 
-        this.sourceInputStream = sourceInputStream;
-        this.logger = l;
+        // As the implementation of read(yte[] b, int off, int len) calls read()
+        // there is a high interest in enclosing the source inputStream in a BufferedInputStream
+        // There is a small penalty if the source inputStream is already buffered but a great advantage if it is not
+        // That is the case for FileInputStream, for instance
+    	this.logger = l;
+        this.sourceInputStream = new BufferedInputStream(sourceInputStream);
         this.buffer = new MultiReaderCircularBuffer<>();
         this.readerId = this.buffer.newReadClient();
         this.forkedOutputStreams = new ArrayList<>();
@@ -29,8 +34,8 @@ public class ForkableInputStream extends InputStream {
 
     public ForkableInputStream(InputStream sourceInputStream, int capacity, Logger l) {
 
-        this.sourceInputStream = sourceInputStream;
-        this.logger = l;
+    	this.logger = l;
+        this.sourceInputStream = new BufferedInputStream(sourceInputStream);
         this.buffer = new MultiReaderCircularBuffer<>(capacity);
         this.readerId = this.buffer.newReadClient();
         this.forkedOutputStreams = new ArrayList<>();
@@ -40,8 +45,8 @@ public class ForkableInputStream extends InputStream {
     // Essentially for test
     protected ForkableInputStream(InputStream sourceInputStream, int capacity, boolean force, Logger l) {
 
-        this.sourceInputStream = sourceInputStream;
-        this.logger = l;
+    	this.logger = l;
+        this.sourceInputStream = new BufferedInputStream(sourceInputStream);
         this.buffer = new MultiReaderCircularBuffer<>(capacity, force);
         this.readerId = this.buffer.newReadClient();
         this.forkedOutputStreams = new ArrayList<>();
@@ -83,6 +88,7 @@ public class ForkableInputStream extends InputStream {
                 }
             }
         }
+
         if (b == -1) {
             // end of source input stream
             writeToForkedOutputStreams();
@@ -92,6 +98,53 @@ public class ForkableInputStream extends InputStream {
         return b;
     }
 
+    // The read(byte[] b, int off, int len) has to be overridden in case the source InputStream has itself overridden it
+    // without calling the read() in its implementation
+    // This ensures that all the bytes are going into the MultiReaderCircularBuffer
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+
+        // Check parameters
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || len < 0 || len > b.length - off) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return 0;
+        }
+
+        int c = read();
+        if (c == -1) {
+            // end of source inputStream
+            // The writeToForkedOutputStreams has already been done in read()
+            return -1;
+        }
+
+        b[off] = (byte)c;
+        int numberOfBytesRead = 1;
+        try {
+            for (; numberOfBytesRead < len ; numberOfBytesRead++) {
+                c = read();
+
+                if (c == -1) {
+                    // end of source inputStream
+                    // The writeToForkedOutputStreams has already been done in read()
+                    break;
+                }
+                b[off + numberOfBytesRead] = (byte)c;
+            }
+        } catch (IOException ignored) {
+            // Treated as  if it were end of file, according to InputStream contract
+        }
+
+        return numberOfBytesRead;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return read(b, 0, b.length);
+    }
+
     @Override
     public void close() throws IOException {
         super.close();
@@ -99,11 +152,50 @@ public class ForkableInputStream extends InputStream {
         if (logger.isLoggable(Level.FINEST)) {
             logger.finest(() -> "Number of bytes read on " + this + " : " + this.nbBytesRead);
             for (ForkedOutputStream forkedOutputStream : forkedOutputStreams) {
-                logger.finest(() -> "Number of bytes written on " + forkedOutputStream + " : " + forkedOutputStream.getNbBytesWritten());
+            	logger.finest(() -> "Number of bytes written on " + forkedOutputStream + " : " + forkedOutputStream.getNbBytesWritten());
             }
         }
     }
-    
+
+    // skip has to be overridden. If not, bytes skipped would be written to outputStreams
+    @Override
+    public long skip(long n) throws IOException {
+
+        if (n <= 0) {
+            return 0;
+        }
+
+        long remaining = n;
+        while (remaining > 0) {
+
+            // Use read() only, NOT read array
+            int b = sourceInputStream.read();
+            if (b == -1) {
+                // end of source input stream
+                break;
+            }
+            remaining--;
+        }
+        return n - remaining;
+    }
+
+    @Override
+    public synchronized void mark(int readlimit) {
+        // As bytes are written to outputStream as they are read, mark cannot be supported
+    }
+
+    // As bytes are written to outputStream as they are read, mark cannot be supported
+    @Override
+    public synchronized void reset() throws IOException {
+        throw new IOException("mark/reset not supported");
+    }
+
+    // As bytes are written to outputStream as they are read, mark cannot be supported
+    @Override
+    public boolean markSupported() {
+        return false;
+    }
+
     @Override
     public int available() {
         return (int)Math.min(buffer.available(readerId), Integer.MAX_VALUE);
@@ -111,18 +203,18 @@ public class ForkableInputStream extends InputStream {
 
     private void writeToForkedOutputStreams() throws IOException {
 
-    	for (ForkedOutputStream forkedOutputStream : forkedOutputStreams) {
-    		Integer b = 0;
-    		long nbBytesWritten = 0;
-    		while (b != null) {
-    			b = buffer.read(forkedOutputStream.readerId);
-    			if ((b != null) && (b != -1)) {
-    				forkedOutputStream.write(b);
-    				nbBytesWritten++;
-    			}
-    		}
-    		forkedOutputStream.addNbBytesWritten(nbBytesWritten);
-    	}
+        for (ForkedOutputStream forkedOutputStream : forkedOutputStreams) {
+                Integer b = 0;
+                long nbBytesWritten = 0;
+                while (b != null) {
+                    b = buffer.read(forkedOutputStream.readerId);
+                    if ((b != null) && (b != -1)) {
+                        forkedOutputStream.write(b);
+                        nbBytesWritten++;
+                    }
+                }
+                forkedOutputStream.addNbBytesWritten(nbBytesWritten);
+        }
     }
 
     private void closeForkedOutputStreams() throws IOException {
@@ -130,18 +222,19 @@ public class ForkableInputStream extends InputStream {
             forkedOutputStream.close();
         }
     }
-    
+
     private static class ForkedOutputStream {
 
         private final int readerId;
-        private OutputStream outputStream;
+        private final OutputStream outputStream;
         private long nbBytesWritten;
 
         public ForkedOutputStream(int readerId, OutputStream outputStream) {
             this.readerId = readerId;
             this.outputStream = outputStream;
+            this.nbBytesWritten = 0;
         }
-        
+
         public void addNbBytesWritten(long n) {
             this.nbBytesWritten = this.nbBytesWritten + n;
         }
--- src/test/java/org/fl/util/io/ForkableInputStreamTest.java
+++ src/test/java/org/fl/util/io/ForkableInputStreamTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
@@ -26,64 +27,95 @@ public class ForkableInputStreamTest {
         InputStream in = stringToInputStream(source);
         ForkableInputStream forkableInputStream = new ForkableInputStream(in, logger);
 
-        // Fork an OutputStream
-        ByteArrayOutputStream forkedOuputStream1 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream1)).isTrue();
+        // Fork an outputStream
+        ByteArrayOutputStream forkedOutputStream1 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream1)).isTrue();
 
         // Now read the source through the ForkableInputStream
-        String readSource = intputStreamToString(forkableInputStream);
+        String readSource = inputStreamToString(forkableInputStream);
 
-        String result1 = forkedOuputStream1.toString();
+        String result1 = forkedOutputStream1.toString();
         assertThat(result1).isEqualTo(source).isEqualTo(readSource);
     }
 
     private static Stream<Arguments> sourcesAndBufferCapacities() {
         String generatedString = generateRandomAlphaNumericString(322);
         String generatedString2 = generateRandomAlphaNumericString(16000);
+        String string3 = "a string for another test in output";
+        String string4 = "1";
         return Stream.of(
-                Arguments.of("a string for another test in output", -1, false),
-                Arguments.of("1", 1024, false),
-                Arguments.of(generatedString, 16, true),
-                Arguments.of(generatedString2, 0, false)
+                Arguments.of(string3, -1, false, stringToInputStream(string3), streamToString),
+                Arguments.of(string4, 1024, false, stringToInputStream(string4), streamToString),
+                Arguments.of(generatedString, 16, true, stringToInputStream(generatedString), streamToString),
+                Arguments.of(generatedString2, 0, false, stringToInputStream(generatedString2), streamToString),
+                Arguments.of(string3, -1, false, new BufferedInputStream(stringToInputStream(string3)), streamToString),
+                Arguments.of(string4, 1024, false, new BufferedInputStream(stringToInputStream(string4)), streamToString),
+                Arguments.of(generatedString, 16, true, new BufferedInputStream(stringToInputStream(generatedString)), streamToString),
+                Arguments.of(generatedString2, 0, false, new BufferedInputStream(stringToInputStream(generatedString2)), streamToString),
+                Arguments.of(string3, -1, false, stringToInputStream(string3), streamToStringThroughBuffer),
+                Arguments.of(string4, 1024, false, stringToInputStream(string4), streamToStringThroughBuffer),
+                Arguments.of(generatedString, 16, true, stringToInputStream(generatedString), streamToStringThroughBuffer),
+                Arguments.of(generatedString2, 0, false, stringToInputStream(generatedString2), streamToStringThroughBuffer),
+                Arguments.of(string3, -1, false, new BufferedInputStream(stringToInputStream(string3)), streamToStringThroughBuffer),
+                Arguments.of(string4, 1024, false, new BufferedInputStream(stringToInputStream(string4)), streamToStringThroughBuffer),
+                Arguments.of(generatedString, 16, true, new BufferedInputStream(stringToInputStream(generatedString)), streamToStringThroughBuffer),
+                Arguments.of(generatedString2, 0, false, new BufferedInputStream(stringToInputStream(generatedString2)), streamToStringThroughBuffer)
         );
     }
+
+    private static final Function<InputStream, String> streamToString = in -> {
+        try {
+            return inputStreamToString(in);
+        } catch (IOException e) {
+            fail("Exception converting Stream to String", e);
+            return null;
+        }
+    };
+
+    private static final Function<InputStream, String> streamToStringThroughBuffer = in -> {
+        try {
+            return inputStreamToStringThroughBuffer(in);
+        } catch (IOException e) {
+            fail("Exception converting Stream to String through buffer", e);
+            return null;
+        }
+    };
 
     @ParameterizedTest
     @MethodSource("sourcesAndBufferCapacities")
     @Tag("UnitTest")
-    void forkFiveOutputStream(String source, int bufferCapacity, boolean forceCapacity) throws IOException {
+    void forkFiveOutputStream(String source, int bufferCapacity, boolean forceCapacity, InputStream sourceInputStream, Function<InputStream, String> streamToString) {
 
-        InputStream in = stringToInputStream(source);
         ForkableInputStream forkableInputStream ;
         if (forceCapacity) {
-            forkableInputStream = new ForkableInputStream(in, bufferCapacity, forceCapacity, logger);
+            forkableInputStream = new ForkableInputStream(sourceInputStream, bufferCapacity, true, logger);
         } else if (bufferCapacity == 0) {
             // try with default constructor
-            forkableInputStream = new ForkableInputStream(in, logger);
+            forkableInputStream = new ForkableInputStream(sourceInputStream, logger);
         } else {
-            forkableInputStream = new ForkableInputStream(in, bufferCapacity, logger);
+            forkableInputStream = new ForkableInputStream(sourceInputStream, bufferCapacity, logger);
         }
 
         // Fork 5 outputStream
-        ByteArrayOutputStream forkedOuputStream1 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream1)).isTrue();
-        ByteArrayOutputStream forkedOuputStream2 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream2)).isTrue();
-        ByteArrayOutputStream forkedOuputStream3 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream3)).isTrue();
-        ByteArrayOutputStream forkedOuputStream4 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream4)).isTrue();
-        ByteArrayOutputStream forkedOuputStream5 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream5)).isTrue();
+        ByteArrayOutputStream forkedOutputStream1 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream1)).isTrue();
+        ByteArrayOutputStream forkedOutputStream2 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream2)).isTrue();
+        ByteArrayOutputStream forkedOutputStream3 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream3)).isTrue();
+        ByteArrayOutputStream forkedOutputStream4 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream4)).isTrue();
+        ByteArrayOutputStream forkedOutputStream5 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream5)).isTrue();
 
         // Now read the source through the ForkableInputStream
-        String readSource = intputStreamToString(forkableInputStream);
+        String readSource = streamToString.apply(forkableInputStream);
 
-        String result1 = forkedOuputStream1.toString();
-        String result2 = forkedOuputStream2.toString();
-        String result3 = forkedOuputStream3.toString();
-        String result4 = forkedOuputStream4.toString();
-        String result5 = forkedOuputStream5.toString();
+        String result1 = forkedOutputStream1.toString();
+        String result2 = forkedOutputStream2.toString();
+        String result3 = forkedOutputStream3.toString();
+        String result4 = forkedOutputStream4.toString();
+        String result5 = forkedOutputStream5.toString();
         assertThat(readSource)
                 .isEqualTo(source)
                 .isEqualTo(result1)
@@ -91,6 +123,67 @@ public class ForkableInputStreamTest {
                 .isEqualTo(result3)
                 .isEqualTo(result4)
                 .isEqualTo(result5);
+    }
+
+    private static Stream<Arguments> skipTestSources() {
+    	String generatedString = generateRandomAlphaNumericString(322);
+        String generatedString2 = generateRandomAlphaNumericString(16000);
+        String string3 = "a string for another test in output";
+        return Stream.of(
+                Arguments.of(string3, stringToInputStream(string3)),
+                Arguments.of(generatedString, stringToInputStream(generatedString)),
+                Arguments.of(generatedString2, stringToInputStream(generatedString2)),
+                Arguments.of(string3, new BufferedInputStream(stringToInputStream(string3))),
+                Arguments.of(generatedString, new BufferedInputStream(stringToInputStream(generatedString))),
+                Arguments.of(generatedString2, new BufferedInputStream(stringToInputStream(generatedString2)))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("skipTestSources")
+    @Tag("UnitTest")
+    void testSkip(String source, InputStream sourceInputStream) throws IOException {
+
+        final int stringLength = source.length();
+
+        if (stringLength > 10) {
+            final int nbFirstReads = stringLength / 3;
+            final int nbSkipped = stringLength / 3;
+
+            String expectedResult = source.substring(0, nbFirstReads) + source.substring(nbFirstReads + nbSkipped);
+
+            ForkableInputStream forkableInputStream = new ForkableInputStream(sourceInputStream, logger);
+
+            // Add 2 forked outputStreams
+            ByteArrayOutputStream forkedOutputStream1 = new ByteArrayOutputStream();
+            assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream1)).isTrue();
+            ByteArrayOutputStream forkedOutputStream2 = new ByteArrayOutputStream();
+            assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream2)).isTrue();
+
+            // Read first part
+            byte[] tab = new byte[expectedResult.length() + 3];
+            int firstReadLength = forkableInputStream.read(tab, 0, nbFirstReads);
+            assertThat(firstReadLength).isEqualTo(nbFirstReads);
+
+            // Skip middle
+            long skippedBytes = forkableInputStream.skip(nbSkipped);
+            assertThat(skippedBytes).isEqualTo(nbSkipped);
+
+            // Read the remaining
+            int remainingLength = forkableInputStream.read(tab, nbFirstReads, expectedResult.length() - nbFirstReads + 1);
+            assertThat(remainingLength).isEqualTo(stringLength - (nbFirstReads + nbSkipped));
+
+            // check string read from inputStream
+            String readString = new String(tab, 0, expectedResult.length());
+            assertThat(readString).isEqualTo(expectedResult);
+
+            // Check that bytes have been written to outputStreams
+            String result1 = forkedOutputStream1.toString();
+            String result2 = forkedOutputStream2.toString();
+            assertThat(result1).isEqualTo(result2).isEqualTo(expectedResult);
+            
+            forkableInputStream.close();
+        }
     }
 
     @Test
@@ -101,16 +194,17 @@ public class ForkableInputStreamTest {
         InputStream in = stringToInputStream(source);
         ForkableInputStream forkableInputStream = new ForkableInputStream(in, logger);
 
-        ByteArrayOutputStream forkedOuputStream1 = new ByteArrayOutputStream();
-        assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream1)).isTrue();
+        ByteArrayOutputStream forkedOutputStream1 = new ByteArrayOutputStream();
+        assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream1)).isTrue();
 
         byte[] tab = new byte[source.length() + 10];
         int length = forkableInputStream.read(tab);
 
         // Check that bytes have been written to outputStream
-        String result1 = forkedOuputStream1.toString();
-        forkableInputStream.close();
+        String result1 = forkedOutputStream1.toString();
         assertThat(result1).isEqualTo(source).hasSize(length);
+        
+        forkableInputStream.close();
     }
 
     @Test
@@ -119,36 +213,42 @@ public class ForkableInputStreamTest {
 
         String source = "a string in output";
         InputStream in = stringToInputStream(source);
-        
+
         // ForkableInputStream can be put in a try with resources
         try (ForkableInputStream forkableInputStream = new ForkableInputStream(in, logger);
-        	 ByteArrayOutputStream forkedOuputStream1 = new ByteArrayOutputStream()) {
+             ByteArrayOutputStream forkedOutputStream1 = new ByteArrayOutputStream()) {
 
-        	assertThat(forkableInputStream.addForkedOutputStream(forkedOuputStream1)).isTrue();
+            assertThat(forkableInputStream.addForkedOutputStream(forkedOutputStream1)).isTrue();
 
-        	int offset = 3;
-        	int readLength = source.length() + 5;
-        	byte[] tab = new byte[offset + readLength + 10];
-        	int length = forkableInputStream.read(tab, offset, readLength);
+            int offset = 3;
+            int readLength = source.length() + 5;
+            byte[] tab = new byte[offset + readLength + 10];
+            int length = forkableInputStream.read(tab, offset, readLength);
 
-        	// check string read from inputStream
+            // check string read from inputStream
             String readString = new String(tab, offset, length);
             assertThat(readString).isEqualTo(source);
-            
-        	// Check that bytes have been written to outputStream
-        	String result1 = forkedOuputStream1.toString();
-        	assertThat(result1).isEqualTo(source).hasSize(length);
+
+            // Check that bytes have been written to outputStream
+            String result1 = forkedOutputStream1.toString();
+            assertThat(result1).isEqualTo(source).hasSize(length);
+
         } catch (Exception e) {
-            fail("Exception with ForkeableInputStream", e);
+            fail("Exception with ForkableInputStream", e);
+            throw(e);  // just to suppress a warning
         }
     }
-    
-    private InputStream stringToInputStream(String s) {
+
+    private static InputStream stringToInputStream(String s) {
         return new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
     }
 
-    private String intputStreamToString(InputStream is) throws IOException {
+    private static String inputStreamToString(InputStream is) throws IOException {
     	return new String (is.readAllBytes(), StandardCharsets.UTF_8);
+    }
+
+    private static String inputStreamToStringThroughBuffer(InputStream is) throws IOException {
+    	return new String ((new BufferedInputStream(is)).readAllBytes(), StandardCharsets.UTF_8);
     }
     
 	private static String generateRandomAlphaNumericString(int stringLength) {
