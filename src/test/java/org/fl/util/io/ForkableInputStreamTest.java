5fabcb6963245ad5dcf9b2954d9fa9b686b9ba5c
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2022-05-22 09:29:36 +0200

    Fix text

--- src/test/java/org/fl/util/io/ForkableInputStreamTest.java
+++ src/test/java/org/fl/util/io/ForkableInputStreamTest.java
@@ -254,12 +254,11 @@ public class ForkableInputStreamTest {
 	private static String generateRandomAlphaNumericString(int stringLength) {
 	    int leftLimit = 48; // numeral '0'
 	    int rightLimit = 122; // letter 'z'
-	    int targetStringLength = 10;
 	    Random random = new Random();
 
 	    return random.ints(leftLimit, rightLimit + 1)
 	      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-	      .limit(targetStringLength)
+	      .limit(stringLength)
 	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 	      .toString();
 	}
