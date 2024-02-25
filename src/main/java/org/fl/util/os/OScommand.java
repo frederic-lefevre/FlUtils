7745628a976a481a43202ca81724931c9ff62246
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2024-02-25 15:53:17 +0100

    Final parameters

--- src/main/java/org/fl/util/os/OScommand.java
+++ src/main/java/org/fl/util/os/OScommand.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2024 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.fl.util.os;
 
 import java.io.IOException;
@@ -6,16 +30,16 @@ import java.util.logging.Logger;
 
 public class OScommand extends Thread {
 
-	private String  command ;
-	private Logger  oLog;
-	private boolean processSuccessfull ; 
-	private boolean waitTermination ;
+	private final String command;
+	private final Logger oLog;
+	private boolean processSuccessfull;
+	private final boolean waitTermination;
 	
 	public OScommand(String cmd, boolean w, Logger l) {
 		super();
-		command 		= cmd ;
-		waitTermination = false ;
-		oLog 			= l ;
+		command = cmd;
+		waitTermination = w;
+		oLog = l;
 	}
 
 	public void run() {
