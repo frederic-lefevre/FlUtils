cfc08800858f4402e9ed3cef9f5efc0e91516adc
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2024-02-25 17:08:14 +0100

    Convert to assertj

--- src/test/java/org/fl/util/swing/ColorHelpersTests.java
+++ src/test/java/org/fl/util/swing/ColorHelpersTests.java
@@ -1,6 +1,30 @@
+/*
+ * MIT License
+
+Copyright (c) 2017, 2024 Frederic Lefevre
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package org.fl.util.swing;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.awt.Color;
 
@@ -11,62 +35,61 @@ class ColorHelpersTests {
 	@Test
 	void testBlue() {
 
-		Color c = ColorHelpers.parse("BLUE") ;
-		assertEquals(c, Color.BLUE) ;
-		
-		c = ColorHelpers.parse("blue") ;
-		assertEquals(c, Color.blue) ;
+		Color c = ColorHelpers.parse("BLUE");
+		assertThat(c).isEqualTo(Color.BLUE);
 
-		c = ColorHelpers.parse("Blue") ;
-		assertNull(c) ;
+		c = ColorHelpers.parse("blue");
+		assertThat(c).isEqualTo(Color.blue);
+
+		c = ColorHelpers.parse("Blue");
+		assertThat(c).isNull();
 	}
 
 	@Test
 	void testGreen() {
 
-		Color c = ColorHelpers.parse("GREEN") ;
-		assertEquals(c, Color.GREEN) ;
-		
-		c = ColorHelpers.parse("green") ;
-		assertEquals(c, Color.green) ;
+		Color c = ColorHelpers.parse("GREEN");
+		assertThat(c).isEqualTo(Color.GREEN);
 
-		c = ColorHelpers.parse("Green") ;
-		assertNull(c) ;
+		c = ColorHelpers.parse("green");
+		assertThat(c).isEqualTo(Color.green);
+
+		c = ColorHelpers.parse("Green");
+		assertThat(c).isNull();
 	}
-	
+
 	@Test
 	void testWrongString() {
 
-		Color c = ColorHelpers.parse("WRONG") ;
-		assertNull(c) ;
+		Color c = ColorHelpers.parse("WRONG");
+		assertThat(c).isNull();
 	}
-	
+
 	@Test
 	void testRGB() {
 
-		Color c = ColorHelpers.parse("#C6C6C6") ;
-		assertNotNull(c) ;
+		Color c = ColorHelpers.parse("#C6C6C6");
+		assertThat(c).isNotNull();
 	}
-	
+
 	@Test
 	void testRGB2() {
 
-		Color c = ColorHelpers.parse("0") ;
-		assertNotNull(c) ;
-		assertEquals(c, Color.black) ;
+		Color c = ColorHelpers.parse("0");
+		assertThat(c).isNotNull().isEqualTo(Color.black);
 	}
-	
+
 	@Test
 	void testEmpy() {
 
-		Color c = ColorHelpers.parse("") ;
-		assertNull(c) ;
+		Color c = ColorHelpers.parse("");
+		assertThat(c).isNull();
 	}
-	
+
 	@Test
 	void testNull() {
 
-		Color c = ColorHelpers.parse(null) ;
-		assertNull(c) ;
+		Color c = ColorHelpers.parse(null);
+		assertThat(c).isNull();
 	}
 }
