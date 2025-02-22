83b987c5870b7bb52e980f2ee6d9f9712d76f523
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2025-02-22 13:56:32 +0100

    Implement java 21 unimplemented methods

--- src/main/java/org/fl/util/io/ForkableInputStream.java
+++ src/main/java/org/fl/util/io/ForkableInputStream.java
@@ -1,24 +1,29 @@
 package org.fl.util.io;
 
 import java.io.BufferedInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ForkableInputStream extends InputStream {
 
-    private final Logger logger;
-    private final int readerId;
-    private final MultiReaderCircularBuffer<Integer> buffer;
-    private final InputStream sourceInputStream;
-    private final InputStream originInputStream;
-    private List<ForkedOutputStream> forkedOutputStreams;
-    private long nbBytesRead;
-    private boolean isClosed = false;
+	private static final int DEFAULT_BUFFER_SIZE = 16384;
+
+	private final Logger logger;
+	private final int readerId;
+	private final MultiReaderCircularBuffer<Integer> buffer;
+	private final InputStream sourceInputStream;
+	private final InputStream originInputStream;
+	private List<ForkedOutputStream> forkedOutputStreams;
+	private long nbBytesRead;
+	private boolean isClosed = false;
 
     public ForkableInputStream(InputStream originInputStream, Logger l) {
 
@@ -151,6 +156,98 @@ public class ForkableInputStream extends InputStream {
         return read(b, 0, b.length);
     }
 
+    /**
+     * The maximum size of array to allocate
+     */
+    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
+    
+    @Override
+    public byte[] readAllBytes() throws IOException {
+        return readNBytes(Integer.MAX_VALUE);
+    }
+    
+    // The readNBytes(int len) has to be overridden in case the source InputStream has itself overridden it
+    // without calling the read() in its implementation
+    // This ensures that all the bytes are going into the MultiReaderCircularBuffer
+    @Override
+    public byte[] readNBytes(int len) throws IOException {
+        if (len < 0) {
+            throw new IllegalArgumentException("len < 0");
+        }
+
+        List<byte[]> bufs = null;
+        byte[] result = null;
+        int total = 0;
+        int remaining = len;
+        int n;
+        do {
+            byte[] buf = new byte[Math.min(remaining, DEFAULT_BUFFER_SIZE)];
+            int nread = 0;
+
+            // read to EOF which may read more or less than buffer size
+            while ((n = read(buf, nread,
+                    Math.min(buf.length - nread, remaining))) > 0) {
+                nread += n;
+                remaining -= n;
+            }
+
+            if (nread > 0) {
+                if (MAX_BUFFER_SIZE - total < nread) {
+                    throw new OutOfMemoryError("Required array size too large");
+                }
+                if (nread < buf.length) {
+                    buf = Arrays.copyOfRange(buf, 0, nread);
+                }
+                total += nread;
+                if (result == null) {
+                    result = buf;
+                } else {
+                    if (bufs == null) {
+                        bufs = new ArrayList<>();
+                        bufs.add(result);
+                    }
+                    bufs.add(buf);
+                }
+            }
+            // if the last call to read returned -1 or the number of bytes
+            // requested have been read then break
+        } while (n >= 0 && remaining > 0);
+
+        if (bufs == null) {
+            if (result == null) {
+                return new byte[0];
+            }
+            return result.length == total ?
+                result : Arrays.copyOf(result, total);
+        }
+
+        result = new byte[total];
+        int offset = 0;
+        remaining = total;
+        for (byte[] b : bufs) {
+            int count = Math.min(b.length, remaining);
+            System.arraycopy(b, 0, result, offset, count);
+            offset += count;
+            remaining -= count;
+        }
+
+        return result;
+    }
+
+    @Override
+    public int readNBytes(byte[] b, int off, int len) throws IOException {
+        Objects.checkFromIndexSize(off, len, b.length);
+
+        int n = 0;
+        while (n < len) {
+            int count = read(b, off + n, len - n);
+            if (count < 0)
+                break;
+            n += count;
+        }
+        return n;
+    }
+    
     @Override
     public void close() throws IOException {
 
@@ -191,6 +288,26 @@ public class ForkableInputStream extends InputStream {
         return n - remaining;
     }
 
+    @Override
+    public void skipNBytes(long n) throws IOException {
+        while (n > 0) {
+            long ns = skip(n);
+            if (ns > 0 && ns <= n) {
+                // adjust number to skip
+                n -= ns;
+            } else if (ns == 0) { // no bytes skipped
+                // read one byte to check for EOS
+                if (read() == -1) {
+                    throw new EOFException();
+                }
+                // one byte read so decrement number to skip
+                n--;
+            } else { // skipped negative or too many bytes
+                throw new IOException("Unable to skip exactly");
+            }
+        }
+    }
+    
     @Override
     public synchronized void mark(int readlimit) {
         // As bytes are written to outputStream as they are read, mark cannot be supported
