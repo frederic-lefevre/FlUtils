68cd54d83764bdea52486eb7cd33f99085526353
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2025-02-23 18:46:46 +0100

    Add asserts on build information

--- src/test/java/org/fl/util/RunningContextTest.java
+++ src/test/java/org/fl/util/RunningContextTest.java
@@ -47,16 +47,16 @@ class RunningContextTest {
 	private static final String LOGGER_NAME = "org.fl.util.test1";
 	
 	@Test
-	void testRunningContextWithNullStringParam() {		
+	void testRunningContextWithNullStringParam() throws JsonProcessingException {		
 		testRunningContextWithNullParam(() -> new RunningContext(null, null, (String)null));
 	}
 
 	@Test
-	void testRunningContextWithNullUriParam() {
+	void testRunningContextWithNullUriParam() throws JsonProcessingException {
 		testRunningContextWithNullParam(() -> new RunningContext(null, null, (URI)null));
 	}
 	
-	private void testRunningContextWithNullParam(Supplier<RunningContext> rcSupplier) {
+	private void testRunningContextWithNullParam(Supplier<RunningContext> rcSupplier) throws JsonProcessingException {
 
 		LogRecordCounter propertiesStorageLogRecordCounter = 
 				FilterCounter.getLogRecordCounter(Logger.getLogger(PropertiesStorage.class.getName()));
@@ -74,6 +74,27 @@ class RunningContextTest {
 		
 		assertThat(advancedProperties.get("buildOs"))
 			.isInstanceOfSatisfying(String.class, buildOs -> buildOs.contains("windows"));
+		
+		JsonNode buildInformation = rc.getBuildInformationAsJson();
+		assertThat(buildInformation).isNotNull();
+		
+		assertThat(buildInformation).isNotEmpty().hasSize(2)
+		.satisfiesExactlyInAnyOrder(
+				buildInfo -> { 
+					assertThat(buildInfo.get("moduleName")).isNotNull();
+					assertThat(buildInfo.get("moduleName").asText()).isEqualTo("org.fl");
+					assertThat(buildInfo.get("buildInformation")).isNotNull();
+					assertThat(buildInfo.get("buildInformation").asText()).isEqualTo("No build information");
+				},
+				buildInfo -> { 
+					assertThat(buildInfo.get("moduleName")).isNotNull();
+					assertThat(buildInfo.get("moduleName").asText()).isEqualTo("org.fl.util");
+					assertThat(buildInfo.get("version")).isNotNull();
+					assertThat(buildInfo.get("version").asText()).isNotEmpty();
+				}
+				);
+		
+		assertThat(rc.getInitializationDate()).isCloseTo(Instant.now(), within(2, ChronoUnit.SECONDS));
 		
 		assertThat(propertiesStorageLogRecordCounter.getLogRecordCount()).isEqualTo(2);
 		assertThat(propertiesStorageLogRecordCounter.getLogRecordCount(Level.WARNING)).isEqualTo(2);
