4b928a66ea7c556a30f0c03418c857e0ee5358f7
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2025-02-25 10:58:43 +0100

    More tests

--- src/test/java/org/fl/util/LogRecordMemoryBufferTest.java
+++ src/test/java/org/fl/util/LogRecordMemoryBufferTest.java
@@ -27,6 +27,7 @@ package org.fl.util;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.stream.Collectors;
@@ -51,27 +52,94 @@ class LogRecordMemoryBufferTest {
 		
 		// Put 10 log records in the buffer
 		int nbLoggedRecords = 10;
-		List<String> logRecordMessagess = IntStream.rangeClosed(1, nbLoggedRecords)
+		List<String> logRecordMessages = IntStream.rangeClosed(1, nbLoggedRecords)
 				.mapToObj(recordNumber -> "Record log " + recordNumber)
 				.collect(Collectors.toList());
 		
-		logRecordMessagess
-			.forEach(recordNumber -> logMemoryBuffer.addLogRecord(new LogRecord(Level.WARNING, "Record log " + recordNumber)));
+		logRecordMessages
+			.forEach(recordMessage -> logMemoryBuffer.addLogRecord(new LogRecord(Level.WARNING, recordMessage)));
 		
 		assertThat(logMemoryBuffer.getMaxLogRecord()).isEqualTo(bufferSize);
 		assertThat(logMemoryBuffer.remainingCapacity()).isEqualTo(bufferSize - nbLoggedRecords);
 		assertThat(logMemoryBuffer.logRecordNumber()).isEqualTo(nbLoggedRecords);
-		assertThat(logMemoryBuffer.getFormattedRecords()).isNotNull().contains(logRecordMessagess);
+		assertThat(logMemoryBuffer.getFormattedRecords()).isNotNull().contains(logRecordMessages);
 		// Log records are still in the buffer
 		assertThat(logMemoryBuffer.logRecordNumber()).isEqualTo(nbLoggedRecords);
 		
 		// Read and clear
-		assertThat(logMemoryBuffer.getAndDeleteFormattedRecords()).isNotNull().contains(logRecordMessagess);
+		assertThat(logMemoryBuffer.getAndDeleteFormattedRecords()).isNotNull().contains(logRecordMessages);
 		assertThat(logMemoryBuffer.getMaxLogRecord())
 			.isEqualTo(logMemoryBuffer.remainingCapacity())
 			.isEqualTo(bufferSize);
 		assertThat(logMemoryBuffer.logRecordNumber()).isZero();
 		assertThat(logMemoryBuffer.getFormattedRecords()).isNotNull().isEmpty();
+	}
+	
+
+	@Test
+	void zeroCapacityShouldThrowExceptio() {
+		assertThatIllegalArgumentException().isThrownBy(() -> new LogRecordMemoryBuffer(0));
+	}
+	
+	@Test
+	void testOverflowAndClear() {
 		
+		int bufferSize = 3;
+		LogRecordMemoryBuffer logMemoryBuffer = new LogRecordMemoryBuffer(bufferSize);
+		
+		assertThat(logMemoryBuffer.getMaxLogRecord())
+			.isEqualTo(logMemoryBuffer.remainingCapacity())
+			.isEqualTo(bufferSize);
+		
+		IntFunction<String> logMessageSupplier = recordNumber -> "Record log " + recordNumber;
+		
+		// Put 3 log records in the buffer to 
+		int nbLoggedRecords = bufferSize;
+		List<String> logRecordMessages = IntStream.rangeClosed(1, nbLoggedRecords)
+				.mapToObj(logMessageSupplier)
+				.collect(Collectors.toList());
+		
+		logRecordMessages
+			.forEach(recordMessage -> logMemoryBuffer.addLogRecord(new LogRecord(Level.WARNING, recordMessage)));
+		
+		assertThat(logMemoryBuffer.remainingCapacity()).isZero();
+		assertThat(logMemoryBuffer.getFormattedRecords()).isNotNull().contains(logRecordMessages);
+		
+		// Log one more message (overflow)
+		logRecordMessages.add(logMessageSupplier.apply(nbLoggedRecords + 1));
+		logMemoryBuffer.addLogRecord(new LogRecord(Level.WARNING, logRecordMessages.getLast()));
+		
+		// Check that all log records are in the buffer except the first one
+		assertThat(logMemoryBuffer.getFormattedRecords()).isNotNull()
+			.contains(logRecordMessages.subList(1, logRecordMessages.size()))
+			.doesNotContain(logRecordMessages.get(0));
+		
+		// Test clear
+		logMemoryBuffer.clear();
+		assertThat(logMemoryBuffer.getMaxLogRecord())
+			.isEqualTo(logMemoryBuffer.remainingCapacity())
+			.isEqualTo(bufferSize);
+	}
+	
+	@Test
+	void testClearAndResize() {
+		
+		int bufferSize = 3;
+		LogRecordMemoryBuffer logMemoryBuffer = new LogRecordMemoryBuffer(bufferSize);
+		
+		assertThat(logMemoryBuffer.getMaxLogRecord())
+			.isEqualTo(logMemoryBuffer.remainingCapacity())
+			.isEqualTo(bufferSize);
+		
+		logMemoryBuffer.addLogRecord(new LogRecord(Level.WARNING, "A message"));
+		
+		assertThat(logMemoryBuffer.remainingCapacity())
+			.isEqualTo(bufferSize - 1);
+		
+		int newSize = 5;
+		logMemoryBuffer.clearAndResize(newSize);
+		assertThat(logMemoryBuffer.getMaxLogRecord())
+			.isEqualTo(logMemoryBuffer.remainingCapacity())
+			.isEqualTo(newSize);
 	}
 }
