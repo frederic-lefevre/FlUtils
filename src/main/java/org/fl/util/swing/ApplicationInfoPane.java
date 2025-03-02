dc2e61c7cc0ebb9f182850dab2500edf65d809f5
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2025-03-02 08:10:27 +0100

    Format code

--- src/main/java/org/fl/util/swing/ApplicationInfoPane.java
+++ src/main/java/org/fl/util/swing/ApplicationInfoPane.java
@@ -52,48 +52,48 @@ public class ApplicationInfoPane extends JPanel {
 			
 	private static final long serialVersionUID = 1L;
 
-	private RunningContext runningContext ;
-	
-	private JTextArea infosText ;
-	private final JScrollPane scrollInfos ;
-	private JCheckBox doIpLookUp ;
-	
+	private RunningContext runningContext;
+
+	private JTextArea infosText;
+	private final JScrollPane scrollInfos;
+	private JCheckBox doIpLookUp;
+
 	public ApplicationInfoPane(RunningContext rc) {
-		super() ;
-		
-		runningContext = rc ;
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)) ;
-		setBorder(BorderFactory.createLineBorder(Color.BLACK,5,true)) ;
-		
-		doIpLookUp = new JCheckBox("Do lookup on IP addresses (may be slow)") ;
-		doIpLookUp.setSelected(false) ;
+		super();
+
+		runningContext = rc;
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK, 5, true));
+
+		doIpLookUp = new JCheckBox("Do lookup on IP addresses (may be slow)");
+		doIpLookUp.setSelected(false);
 		doIpLookUp.addActionListener(new SetLookUpListener());
-		
-		infosText = new JTextArea() ;
-		
+
+		infosText = new JTextArea();
+
 		// to always be on the top of the scrollPane
 		DefaultCaret caret = (DefaultCaret) infosText.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
-		
-		scrollInfos = new JScrollPane(infosText) ;	
 
-		add(doIpLookUp) ;
-		add(scrollInfos) ;	
+		scrollInfos = new JScrollPane(infosText);
+
+		add(doIpLookUp);
+		add(scrollInfos);
 	}
-	
+
 	public void setInfos() throws JsonProcessingException {
-		setInfos(doIpLookUp.isSelected()) ;
+		setInfos(doIpLookUp.isSelected());
 	}
-	
+
 	private void setInfos(boolean withLookUp) throws JsonProcessingException {
-		JsonNode infosJson = runningContext.getApplicationInfo(withLookUp) ;		
-		infosText.setText(JsonUtils.jsonPrettyPrint(infosJson)) ;
-		
+		JsonNode infosJson = runningContext.getApplicationInfo(withLookUp);
+		infosText.setText(JsonUtils.jsonPrettyPrint(infosJson));
+
 		scrollInfos.getVerticalScrollBar().setValue(0);
-		JViewport vp = scrollInfos.getViewport() ;
-		vp.setViewPosition(new Point(0,0));
+		JViewport vp = scrollInfos.getViewport();
+		vp.setViewPosition(new Point(0, 0));
 	}
-	
+
 	private class SetLookUpListener implements ActionListener {
 
 		@Override
@@ -101,12 +101,12 @@ public class ApplicationInfoPane extends JPanel {
 			if (doIpLookUp.isSelected()) {
 				infosText.setText("Updating...");
 				try {
-					setInfos(true) ;
+					setInfos(true);
 				} catch (JsonProcessingException e1) {
 					logger.log(Level.SEVERE, "Exception setting Application pane infos", e);
 				}
 			}
 		}
-		
+
 	}
 }
