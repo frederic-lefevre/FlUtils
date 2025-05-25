a8c8dce2817216b741f5c4c8486ebdf2e8802fc9
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-05-25 20:37:20 +0200

    Modify assertj imports

--- src/test/java/org/fl/util/response/ResponseTest.java
+++ src/test/java/org/fl/util/response/ResponseTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
 
-Copyright (c) 2017, 2024 Frederic Lefevre
+Copyright (c) 2017, 2025 Frederic Lefevre
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -37,8 +37,7 @@ import java.util.stream.Collectors;
 import org.fl.util.LoggerCounter;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.*;
 
 public class ResponseTest {
 
